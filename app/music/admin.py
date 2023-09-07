2a2b935dfe41230dd4eb5f36062fc78450a6197c
Oleg Remizov <job.rem1zoff@gmail.com>
2023-09-07 12:54:02 +0300

    models created

--- app/music/admin.py
+++ app/music/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Performer, Album, Song
 
-# Register your models here.
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('name', 'performer', 'album',  'position', )
+    list_filter = ('name', )
+
+
+
+admin.site.register(Performer)
+admin.site.register(Album)
+admin.site.register(Song, SongAdmin)
--- app/music/models.py
+++ app/music/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Performer(models.Model):
+    name  = models.CharField(max_length=255, unique=True, verbose_name='Исполнитель')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Исполнитель'
+        verbose_name_plural = 'Исполнители'
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255, null=True, verbose_name='Имя альбома')
+    performer = models.ForeignKey(Performer, on_delete=models.CASCADE, verbose_name='Автор')
+    date_relize = models.IntegerField('Год релиза')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Альбом'
+        verbose_name_plural = 'Альбомы'
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Песня')
+    performer = models.ForeignKey(Performer, on_delete=models.SET_NULL, 
+                                  null=True, verbose_name='Исполнитель')
+    # album = models.ManyToManyField(Album,  related_name='songs', verbose_name='Альбом')
+    album = models.ForeignKey(Album, verbose_name='Альбом', on_delete=models.CASCADE)
+    position = models.IntegerField(blank=True, null=True, verbose_name='Позиция')
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Песня'
+        verbose_name_plural = 'Песни'
